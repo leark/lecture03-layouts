6aefb7a677e1d1b89140ce776964b49baf3085d8
Seung Lee <shl7@uw.edu>
2017-04-04 15:24:53 -0700

    added lecture exercises

--- app/src/main/java/edu/uw/layoutdemo/MainActivity.java
+++ app/src/main/java/edu/uw/layoutdemo/MainActivity.java
@@ -2,6 +2,10 @@ package edu.uw.layoutdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "LAYOUT_DEMO";
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflator = getLayoutInflater();
+                // inflator.inflate();
+
+//                ImageView img = (ImageView)findViewById(R.id.imgCat);
+//                img.setImageResource(R.drawable.android_maneki);
+            }
+        });
     }
 }
